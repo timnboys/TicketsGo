2b186120687772362bc49bc1f579d05950383bcd
Dot-Rar <dotrar@protonmail.com>
2019-06-26 19:49:28 +0100

    build in hasadmin

--- bot/command/commandcontext.go
+++ bot/command/commandcontext.go
@@ -37,9 +37,21 @@ func (ctx *CommandContext) GetPermissionLevel(ch chan utils.PermissionLevel) {
 }
 
 func (ctx *CommandContext) ChannelMemberHasPermission(channel, user string, permission utils.Permission, ch chan bool) {
-	utils.ChannelMemberHasPermission(ctx.Session, ctx.Guild, channel, user, permission, ch)
+	hasAdmin := make(chan bool)
+	go utils.ChannelMemberHasPermission(ctx.Session, ctx.Guild, channel, user, utils.Administrator, hasAdmin)
+	if <-hasAdmin {
+		ch <- true
+	} else {
+		utils.ChannelMemberHasPermission(ctx.Session, ctx.Guild, channel, user, permission, ch)
+	}
 }
 
 func (ctx *CommandContext) MemberHasPermission(user string, permission utils.Permission, ch chan bool) {
-	utils.MemberHasPermission(ctx.Session, ctx.Guild, user, permission, ch)
+	hasAdmin := make(chan bool)
+	go utils.MemberHasPermission(ctx.Session, ctx.Guild, user, utils.Administrator, hasAdmin)
+	if <-hasAdmin {
+		ch <- true
+	} else {
+		utils.MemberHasPermission(ctx.Session, ctx.Guild, user, permission, ch)
+	}
 }
