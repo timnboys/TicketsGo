0eea727f2d8a2a2317a7579fdff9b8f2442c8ae8
Dot-Rar <dotrar@protonmail.com>
2019-06-28 13:02:32 +0100

    stop perms being applied to all guilds

--- database/permissions.go
+++ database/permissions.go
@@ -114,11 +114,7 @@ func RemoveAdmin(guild string, user string) {
 		return
 	}
 
-	node := Permissions{
-		GuildId: guildId,
-		UserId: userId,
-	}
-	Db.Model(&node).Update("ISADMIN", false)
+	Db.Where(Permissions{GuildId: guildId, UserId: userId}).Update("ISADMIN", false)
 }
 
 func RemoveSupport(guild string, user string) {
@@ -130,11 +126,7 @@ func RemoveSupport(guild string, user string) {
 		return
 	}
 
-	node := Permissions{
-		GuildId: guildId,
-		UserId:  userId,
-	}
-	Db.Model(&node).Updates(map[string]interface{}{
+	Db.Where(Permissions{GuildId: guildId, UserId: userId}).Updates(map[string]interface{}{
 		"ISADMIN": false,
 		"ISSUPPORT": false,
 	})
