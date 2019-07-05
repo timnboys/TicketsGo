22181c17f544aae2db0ea712a6ceb7e05acda52e
Dot-Rar <dotrar@protonmail.com>
2019-07-05 02:10:26 +0100

    Update addadmin.go

--- bot/command/addadmin.go
+++ bot/command/addadmin.go
@@ -49,11 +49,9 @@ func (AddAdminCommand) Execute(ctx CommandContext) {
 
 	for _, channelId := range <-openTicketsChan {
 		var overwrites []*discordgo.PermissionOverwrite
-		ch, err := ctx.Session.State.Channel(ctx.Channel); if err != nil {
-			ch, err = ctx.Session.Channel(ctx.Channel); if err != nil {
-				log.Error(err.Error())
-				return
-			}
+		ch, err := ctx.Session.Channel(ctx.Channel); if err != nil {
+                        log.Error(err.Error())
+			return
 		}
 
 		overwrites = ch.PermissionOverwrites
