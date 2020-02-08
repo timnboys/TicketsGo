f9aaac523ce493f80e60c68c958265cfd73c8595
Dot-Rar <dotrar@protonmail.com>
2020-02-08 19:05:39 +0000

    fix nil

--- database/channels.go
+++ database/channels.go
@@ -1,9 +1,5 @@
 package database
 
-import (
-	"github.com/TicketsBot/GoPanel/database"
-)
-
 type Channel struct {
 	ChannelId int64  `gorm:"column:CHANNELID"`
 	GuildId   int64  `gorm:"column:GUILDID"`
@@ -23,7 +19,7 @@ func StoreChannel(channelId, guildId int64, name string, channelType int) {
 		Type:      channelType,
 	}
 
-	database.Database.Where(&Channel{ChannelId:channelId}).Assign(&channel).FirstOrCreate(&Channel{})
+	Db.Where(&Channel{ChannelId:channelId}).Assign(&channel).FirstOrCreate(&Channel{})
 }
 
 func DeleteChannel(channelId int64) {
