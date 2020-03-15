e3ca00d2a6ef3b21f4f7bf86cf33e6ad9433b8b2
Dot-Rar <dotrar@protonmail.com>
2020-03-15 12:47:57 +0000

    comment

--- cache/guildcache.go
+++ cache/guildcache.go
@@ -16,6 +16,7 @@ func (c *RedisClient) GetGuildOwner(id string, res chan string) {
 	key := fmt.Sprintf("tickets:guilds:%s", id)
 	response := c.HGet(key, "owner")
 
+	// TODO: Publish message to shard to repopulate cache if not present
 	if response.Err() != nil {
 		sentry.Error(response.Err())
 		res <- ""
@@ -29,6 +30,7 @@ func (c *RedisClient) GetGuildName(id string, res chan string) {
 	key := fmt.Sprintf("tickets:guilds:%s", id)
 	response := c.HGet(key, "name")
 
+	// TODO: Publish message to shard to repopulate cache if not present
 	if response.Err() != nil {
 		sentry.Error(response.Err())
 		res <- ""
