58db5c460f641dbf001a583a35d60a0acaddc124
Dot-Rar <dotrar@protonmail.com>
2020-05-06 15:26:31 +0100

    bump gdl

--- bot/listeners/readylistener.go
+++ bot/listeners/readylistener.go
@@ -3,9 +3,8 @@ package listeners
 import (
 	"github.com/rxdn/gdl/gateway"
 	"github.com/rxdn/gdl/gateway/payloads/events"
-	"github.com/rxdn/gdl/objects/user"
 )
 
 func OnReady(s *gateway.Shard, e *events.Ready) {
-	_ = s.UpdateStatus(user.BuildStatus(user.ActivityTypePlaying, "DM for help | t!help"))
+	//_ = s.UpdateStatus(user.BuildStatus(user.ActivityTypePlaying, "DM for help | t!help"))
 }
